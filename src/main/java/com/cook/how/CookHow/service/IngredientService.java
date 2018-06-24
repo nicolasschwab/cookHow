fbc10a808967a09d75501e86f76fbd0eb8424e98
nicolasschwab <nicolasschwab2@hotmail.com>
2018-06-24 02:20:00 -0300

    Adding Dozer mapping in Service Layer

--- src/main/java/com/cook/how/CookHow/service/IngredientService.java
+++ src/main/java/com/cook/how/CookHow/service/IngredientService.java
@@ -2,18 +2,29 @@ package com.cook.how.CookHow.service;
 
 import com.cook.how.CookHow.dto.Ingredient;
 import com.cook.how.CookHow.repository.IngredientRepository;
+import com.cook.how.CookHow.util.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class IngredientService {
 
     @Autowired
     private IngredientRepository ingredientRepository;
 
+    @Autowired
+    private Mapper mapper;
+
     public List<Ingredient> getIngredientsStartingWith(String partialName){
         List<com.cook.how.CookHow.model.Ingredient> ingredients = ingredientRepository.findByNameStartingWith(partialName);
+        return mapper.mapList(ingredients, Ingredient.class);
+    }
 
+    public List<Ingredient> getAll(){
+        List<com.cook.how.CookHow.model.Ingredient> ingredients = ingredientRepository.findAll();
+        return mapper.mapList(ingredients, Ingredient.class);
     }
 
 }
