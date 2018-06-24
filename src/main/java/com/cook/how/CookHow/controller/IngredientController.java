9fd598b4546920f15152a558eeed3581ee17ed3e
nicolasschwab <nicolasschwab2@hotmail.com>
2018-06-24 02:21:33 -0300

    inferAndGet and GetAll  ingredient Controller

--- src/main/java/com/cook/how/CookHow/controller/IngredientController.java
+++ src/main/java/com/cook/how/CookHow/controller/IngredientController.java
@@ -1,26 +1,33 @@
 package com.cook.how.CookHow.controller;
 
 import com.cook.how.CookHow.dto.Ingredient;
-import com.cook.how.CookHow.repository.IngredientRepository;
 import com.cook.how.CookHow.service.IngredientService;
 import com.cook.how.CookHow.validator.IngredientNormalizerValidator;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ingredient")
 public class IngredientController {
 
-    @Autowired
     private IngredientService ingredientService;
 
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
     @GetMapping("/inferAndGet")
-    public String inferAndGet(Ingredient ingredient){
-        IngredientNormalizerValidator.validateInferAndGet(ingredient);
-        ingredientService.getIngredientsStartingWith(ingredient.name);
-        return "";
+    public @ResponseBody
+    List<Ingredient> inferAndGet(@RequestParam String ingredientName){
+        //IngredientNormalizerValidator.validateInferAndGet(ingredient);
+        return ingredientService.getIngredientsStartingWith(ingredientName);
+    }
+
+    @GetMapping("/getAll")
+    public @ResponseBody List<Ingredient> getAll(){
+        List<Ingredient> p = ingredientService.getAll();
+        return p;
     }
 
 }
